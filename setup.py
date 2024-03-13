8a488f50c0d65e25b126790e8f20f8c118cfaec7
mrqadeer <mrqadeer1231122@gmail.com>
2024-03-13 14:25:01 +0500

    git init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is NLP Project to recommend movies to user',
+    author='Qadeer Ahmad',
+    license='',
+)
