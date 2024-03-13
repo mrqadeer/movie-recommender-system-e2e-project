701c7b5c116d68f0027a3d00fc438195da7c272f
mrqadeer <mrqadeer1231122@gmail.com>
2024-03-14 01:29:52 +0500

    stereamlit gui and some run files changes

--- run.py
+++ run.py
@@ -3,9 +3,12 @@ import subprocess
 
 def run_files_from_yaml(yaml_file):
     run=yaml.safe_load(open(yaml_file))
-
+    for file_path in run['requirements']:
+        subprocess.run(['pip','install','-r',file_path])
     for file_path in run['files_to_run']:
         subprocess.run(['python', file_path])
+    for file_path in run['streamlit_app']:
+        subprocess.run(['python','-m','streamlit','run',file_path])
 if __name__ == "__main__":
     run_files_from_yaml('run.yaml')
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,34 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+st.set_page_config("Movie Recommendation System",page_icon='🎥')
+
+class App:
+    def __init__(self) -> None:
+        pass
+    
+    def run(self):
+        with st.sidebar:
+            app=option_menu(
+                menu_title='Introduction',
+                options=['Home','Prediction'],
+                icons=['house-heart','codepen'],
+                menu_icon='info',
+                default_index=0,
+                styles={
+                    "container": {"padding": "15!important", "background-color": '#040134'},
+                    "icon": {"color": "white ", "font-size": "20px"},
+                    "nav-link": {"color": "white", "font-size": "20px",'font-weight':'bold', 
+                                 "text-align": "left", "margin": "0px",
+                                 "--hover-color": "magenta"},
+                    "nav-link-selected": {"background-color": "#02ab21"}
+                }
+            )
+        if app=='Home':
+            st.write("Home")
+        if app=='Prediction':
+            st.write("Prediction")
+
+            
+if __name__=='__main__':
+    app=App()
+    app.run()
